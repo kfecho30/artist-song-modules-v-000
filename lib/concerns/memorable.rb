e81ef8b6fd2c3d0a132bed5208816903fc83abcd
Kelsey Fecho <kfecho30@gmail.com>
2018-03-15 19:03:23 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,4 +1,5 @@
 module Memorable
+  module ClassMethods
     def reset_all
       self.all.clear
     end
@@ -6,4 +7,9 @@ module Memorable
     def count
       self.all.count
     end
+  end
+  module InstanceMethods
+    def initialize
+    end
+  end
 end
